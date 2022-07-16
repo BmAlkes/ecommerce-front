7a758f95fa894a6a629cc6a2872c6ea75871c8fa
bmalkes <bmalkes@gmail.com>
2022-07-16 20:43:24 +0300

    feat: productPage

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,13 +1,56 @@
 import React from "react";
 import Header from "../../components/Header";
 import { Container } from "./styles";
+import nike from "../../assets/imgs/nike2.png";
+import SquareProduct from "../../components/squareShoes";
 
 const Products = () => {
   return (
     <Container>
       <Header />
-      <div className=" dividerSection">
+      <div className="dividerSection">
         <h2>Product</h2>
+      </div>
+      <div className="optionsLine">
+        <div>
+          <select name="colection" id="colection">
+            <option value="coletion" selected disabled hidden>
+              collection
+            </option>
+            <option value="Men">Men</option>
+            <option value="Woman">Woman</option>
+            <option value="kids">Kids</option>
+            <option value="all">All</option>
+          </select>
+          <select name="Size" id="Size">
+            <option value="Size" selected disabled hidden>
+              Size
+            </option>
+            <option value="">45</option>
+            <option value="">44</option>
+            <option value="">43</option>
+            <option value="all">All</option>
+          </select>
+        </div>
+        <div>
+          <select name="sort" id="sort">
+            <option value="sort" selected disabled hidden>
+              sort by: cheapest
+            </option>
+            <option value="">45</option>
+            <option value="">44</option>
+            <option value="">43</option>
+            <option value="all">All</option>
+          </select>
+        </div>
+      </div>
+      <div className="squareProduct">
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
+        <SquareProduct text={"Air Max 90"} price={300} img={nike} />
       </div>
     </Container>
   );
--- src/pages/Products/styles.js
+++ src/pages/Products/styles.js
@@ -1,5 +1,42 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  max-width: 1170px;
+  margin: 0 auto;
+
+  .dividerSection {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 15px;
+    border-bottom: 3px solid ${(props) => props.theme.colors.line};
+  }
+  h2 {
+    color: ${(props) => props.theme.colors.textColor};
+    font-size: 40px;
+  }
+  .optionsLine {
+    display: flex;
+    justify-content: space-between;
+    select {
+      border: none;
+      font-size: 18px;
+      margin-right: 20px;
+    }
+  }
+  .squareProduct {
+    margin: 30px auto;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    flex-wrap: wrap;
+  }
+  @media (max-width: 768px) {
+    .squareProduct {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
 
 export { Container };
